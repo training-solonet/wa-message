8409b453a73880654d20f4c008e914cf6ae1dea8
Edy kurniawan <edy.kurniawan280898@gmail.com>
2024-11-11 12:36:08 +0700

    broadcast video

--- src/routers/messageRoute.js
+++ src/routers/messageRoute.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import whatsappClient from "../services/WhatsappClient.js";
 import pkg from "whatsapp-web.js";
 import Informan from "../models/Informan.js";
+import fs from "fs";
 const { MessageMedia } = pkg;
 
 const router = new express.Router();
@@ -13,33 +14,76 @@ router.get("/", (req, res) => {
   res.send("API is running");
 });
 
-router.post("/message", async (req, res) => {
-  const { phoneNumber, message, mediaUrl, mediaType } = req.body;
-
+router.get("/get-groups-members", async (req, res) => {
+  // Ganti dengan ID grup Anda
+  const groupId = "120363225953135948@g.us";
+  const groupMembers = [];
   try {
-    if (mediaUrl) {
-      const response = await axios.get(mediaUrl, {
-        responseType: "arraybuffer",
+    const chat = await whatsappClient.getChatById(groupId);
+    if (chat.isGroup) {
+      // Mengakses daftar peserta langsung dari properti `participants`
+      chat.participants.forEach((participant) => {
+        groupMembers.push(participant.id.user);
       });
-      const mediaData = Buffer.from(response.data).toString("base64");
-      const mimeType = response.headers["content-type"];
-      const filename = mediaType === "video" ? "video.mp4" : "image.jpg";
+    }
+    res.send({ groupMembers });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
 
-      const media = new MessageMedia(mimeType, mediaData, filename);
+router.post("/message", async (req, res) => {
 
-      await whatsappClient.sendMessage(phoneNumber, media, {
+  const message = "Semangat siang! 🔥 Mari sebagai relawan penggerak udara, mari kita bantu sebarkan konten-konten berikut. Andika- Hendi Menang Mutlak!";
+
+  // Memuat file video langsung dari path
+  const videoMedia = MessageMedia.fromFilePath('./src/assets/video/2.mp4'); // Sesuaikan dengan path file Anda
+
+  // get all informan
+  const informan = await Informan.findAll({
+    attributes: ["nama", "no_hp"],
+    limit: 150,
+    offset: 200,
+  });
+
+  // change no_hp to 62
+  const phone_numbers = informan.map((data) => {
+    if (data.no_hp.startsWith("0")) {
+      return data.no_hp.replace("0", "62");
+    }
+    return data.no_hp;
+  });
+
+  // except number
+  const exceptNumber = ["6282135922585"];
+
+  // filter phone_numbers
+  const filteredPhoneNumbers = phone_numbers.filter((data) => {
+    return !exceptNumber.includes(data);
+  });
+
+  // Mengirim video
+  let number = 0;
+  for (const phone_number of filteredPhoneNumbers) {
+    number++;
+    try {
+      const phoneSuffix = `${
+        phone_number
+      }@c.us`;
+      // send message
+      await whatsappClient.sendMessage(phoneSuffix, videoMedia, {
         caption: message,
       });
-    } else {
-      await whatsappClient.sendMessage(phoneNumber, message);
+      console.log(`Message sent to ${number} of ${phone_numbers.length}`);
+      // Delay between messages
+      const randomDelay = Math.floor(Math.random() * 3000) + 1000;
+      await delay(randomDelay);
+    } catch (error) {
+      console.error("Error sending message:", error);
     }
-    res.send({ status: "Message sent successfully" });
-  } catch (error) {
-    console.error("Error sending message:", error);
-    res
-      .status(500)
-      .send({ status: "Failed to send message", error: error.message });
   }
+
+  res.send({ status: "Message sent successfully" });
 });
 
 router.post("/broadcast", async (req, res) => {
@@ -48,7 +92,7 @@ router.post("/broadcast", async (req, res) => {
   // get all informan
   const informan = await Informan.findAll({
     attributes: ["nama", "no_hp"],
-    limit: 100,
+    limit: 5,
     offset: 0,
   });
 
@@ -60,11 +104,18 @@ router.post("/broadcast", async (req, res) => {
     return data.no_hp;
   });
 
+  // except number
+  const exceptNumber = ["6282135922585"];
+
+  // filter phone_numbers
+  const filteredPhoneNumbers = phone_numbers.filter((data) => {
+    return !exceptNumber.includes(data);
+  });
+
   // Menyimpan hasil log broadcast
   let logData = [];
 
   if (mediaUrl) {
-
     const response = await axios.get(mediaUrl, {
       responseType: "arraybuffer",
     });
@@ -74,13 +125,16 @@ router.post("/broadcast", async (req, res) => {
 
     const media = new MessageMedia(mimeType, mediaData, filename);
 
-    for (const phone_number of phone_numbers) {
+    let number = 0;
+    for (const phone_number of filteredPhoneNumbers) {
+      number++;
       try {
         const phoneSuffix = `${phone_number}@c.us`;
         // send message
         await whatsappClient.sendMessage(phoneSuffix, media, {
           caption: message,
         });
+        console.log(`Message sent to ${number} of ${phone_numbers.length}`);
         // Menyimpan log broadcast
         logData.push({ phone_number, status: "Message sent successfully" });
 
@@ -98,7 +152,7 @@ router.post("/broadcast", async (req, res) => {
       }
     }
   } else {
-    for (const phone_number of phone_numbers) {
+    for (const phone_number of filteredPhoneNumbers) {
       try {
         const phoneSuffix = `${phone_number}@c.us`;
         // send message
@@ -124,10 +178,12 @@ router.post("/broadcast", async (req, res) => {
   // Menyimpan log broadcast ke file
   const timestamp = new Date().toISOString();
   fs.writeFileSync(
-    "broadcast_log.json",
+    "broadcast_log2.json",
     JSON.stringify({ timestamp, logData }, null, 2)
   );
   console.log("Log broadcast tersimpan di broadcast_log.json");
+
+  res.send({ status: "Broadcast completed", logData });
 });
 
 export default router;
--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -3,7 +3,10 @@ const { Client, LocalAuth } = pkg;
 import qrcode from "qrcode-terminal";
 
 const whatsappClient = new Client({
-  puppeteer: { headless: true },
+  puppeteer: { 
+    headless: false,
+    executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+   },
   authStrategy: new LocalAuth(),
   logLevel: 'verbose',
 });
@@ -15,7 +18,20 @@ whatsappClient.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-whatsappClient.on("ready", () => console.log("client is ready"));
+whatsappClient.on('ready', async () => {
+  console.log('Client is ready!');
+
+  // Mendapatkan semua chat
+  const chats = await whatsappClient.getChats();
+
+  // Filter untuk mendapatkan chat grup saja
+  const groups = chats.filter(chat => chat.isGroup);
+
+  console.log('Grup WhatsApp:');
+  groups.forEach(group => {
+      console.log(`Nama Grup: ${group.name} - ID Grup: ${group.id._serialized}`);
+  });
+});
 
 whatsappClient.on("message", async (msg) => {
   try {
@@ -38,6 +54,10 @@ whatsappClient.on("disconnected", (reason) => {
     console.log("Attempting to reconnect...");
     await whatsappClient.initialize();
   }, 10000); 
+});
+
+whatsappClient.on('error', (error) => {
+  console.error('Client encountered an error:', error);
 });
 
 export default whatsappClient;
